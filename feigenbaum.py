9cc9fc3edd98fb5b4d2f948abb4e5a80a1fc0f53
Felipe Contreras <hobber.mallow@gmail.com>
2019-08-22 13:24:35 -0500

    feig

---
+++ feigenbaum.py
@@ -0,0 +1,25 @@
+__author__ = 'fhca'
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+
+def logistica(r, x0=0.2, nvalores=1000):
+    evalua = np.frompyfunc(lambda x, _: r*x*(1-x), 2, 1)
+    orb = np.empty(nvalores)
+    orb[0] = x0
+    return evalua.accumulate(orb, dtype=np.object).astype(np.float)
+
+
+def feigenbaum(f, minimo=0, maximo=4, nr=5000, n=3000):
+    erres = np.linspace(minimo, maximo, nr, endpoint=True)
+    plt.figure(figsize=(10, 8))
+    for i, r in enumerate(erres):
+        "x=[r,r,r,...] n-veces"
+        plt.plot(np.full(n, r), f(r, x0=f(r)[-1], nvalores=n),
+                 ',', color=cm.coolwarm(i/nr))
+    plt.show()
+
+
+feigenbaum(logistica, 2, 4)
